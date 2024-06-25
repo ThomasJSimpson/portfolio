03c60290caec2252f408cf384634b8c143fb740f
Thomas Berenger <berenger.thomas@gmail.com>
2024-06-25 22:32:31 +0200

    deploy config2

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,28 @@ import WelcomePage from "./pages/WelcomePage.jsx";
 import ProjectPage from "./pages/ProjectPage.jsx";
 import "./App.css";
 import ScrollToTop from "./components/ScrollToTop.jsx";
-const router = createBrowserRouter([
-  {
-    element: <AppLayout />,
-    children: [
-      {
-        path: "/",
-        element: <WelcomePage />,
-      },
-      {
-        path: "/project/:projectId",
-        element: (
-          <>
-            <ScrollToTop /> <ProjectPage />
-          </>
-        ),
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      element: <AppLayout />,
+      children: [
+        {
+          path: "/",
+          element: <WelcomePage />,
+        },
+        {
+          path: "/project/:projectId",
+          element: (
+            <>
+              <ScrollToTop /> <ProjectPage />
+            </>
+          ),
+        },
+      ],
+    },
+  ],
+  { basename: "/portfolio" }
+);
 
 function App() {
   return <RouterProvider router={router} />;
